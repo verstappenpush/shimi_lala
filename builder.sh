564f4eabd35f14ea9d18e7e2ad75eb06bb9b7159
verstappenpush <thiagojurema445@gmail.com>
2026-05-10 16:28:00 -0300

    Update builder.sh

--- builder.sh
+++ builder.sh
@@ -29,10 +29,10 @@ mkdir lineage2
 cd lineage2
 
 # init repo
-repo init -u https://github.com/DerpFest-AOSP/manifest.git -b 14 --depth=1
+repo init -u https://github.com/crDroid-Resurrected/android.git -b 13.0 --git-lfs --depth=1
 
 # clone manifest
-git clone https://github.com/Frost444/local_manifests_odessa.git -b main .repo/local_manifests
+git clone https://github.com/verstappenpush/local_manifests_odessa -b main .repo/local_manifests
 
 # sync
 repo sync -c --no-clone-bundle --optimized-fetch --prune --force-sync -j10
